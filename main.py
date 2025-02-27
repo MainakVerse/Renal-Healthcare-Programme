2577cc3c95628ea17b87a79beb4d48f0216309cf
MAINAK CHAUDHURI <mainakchaudhuri671@gmail.com>
2025-02-27 10:08:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from Tabs import diagnosis, home, result,  kc, talk2doc
 # Configure the app
 st.set_page_config(
     page_title = 'Renal Healthcare Program',
-    page_icon = '🥯',
+    page_icon = '🫗',
     layout = 'wide',
     initial_sidebar_state = 'auto'
 )
